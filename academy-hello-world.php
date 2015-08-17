7ae510dd0b4a4dd04876a30e5938e2c5b8d610e2
Luke Carbis <luke@xwp.co>
2015-08-17 23:21:53 +1000

    Add base plugin class

---
+++ academy-hello-world.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Hello World
+ * Description: Inserts Hello World into your Admin header.
+ * Version: 1.0
+ * Author: Luke Carbis
+ * Author URI: http://academy.xwp.co
+ * License: GPLv2+
+ */
+
+class Academy_Hello_World {
+	function __construct() {
+		add_action( 'admin_notices', array( $this, 'output_admin_notices' ) );
+	}
+
+	function output_admin_notices() {
+		echo wp_kses_post( sprintf( '<p class="academy-hello-world>%s</p>', __( 'Hello World', 'academy_hello_world' ) ) );
+	}
+}
+$GLOBALS['academy_hello_world'] = new Academy_Hello_world();
